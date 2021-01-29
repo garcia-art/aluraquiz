309ad5448c444c5e451d594dd2bf08faef14ac60
Lucas  Garcia <lucasgarcia.art@gmail.com>
2021-01-29 16:03:51 -0300

    v2 - pt2

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import React from 'react';
 import { useRouter } from 'next/router';
 import db from '../db.json';
@@ -7,6 +6,9 @@ import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizLogo from '../src/components/QuizLogo';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 // const Title = styled.h1`
 //   font-size: 50px;
@@ -22,16 +24,16 @@ import QuizLogo from '../src/components/QuizLogo';
 // `;
 
 // não é React, é JS puro
-const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+// const QuizContainer = styled.div`
+//   width: 100%;
+//   max-width: 350px;
+//   padding-top: 45px;
+//   margin: auto 10%;
+//   @media screen and (max-width: 500px) {
+//     margin: auto;
+//     padding: 15px;
+//   }
+// `;
 
 export default function Home() {
   const router = useRouter();
@@ -47,26 +49,27 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function (infosDoEvento) {
+            <form onSubmit={(infosDoEvento) => {
               infosDoEvento.preventDefault();
 
               router.push(`/quiz?name=${name}`);
               // router manda para próxima página
             }}
             >
-              <input
-                onChange={function (infosDoEvento) {
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => {
                   // name = infosDoEvento.target.value;
                   setName(infosDoEvento.target.value);
                   // State
                 }}
                 placeholder="Coloque seu nome, vassalo! "
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-                {' '}
-                {name}
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
