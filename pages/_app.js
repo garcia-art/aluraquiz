f10021d50ec023da2312b5998eb303e9ace19b28
Lucas  Garcia <lucasgarcia.art@gmail.com>
2021-01-29 13:39:52 -0300

    v2

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import db from '../db.json'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+import React from 'react';
+import db from '../db.json';
+
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
@@ -17,23 +20,47 @@ const GlobalStyle = createGlobalStyle`
   html, body {
     min-height: 100vh;
   }
-  #__next {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
+    #__next {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
   }
 `;
 
-const theme = db.theme;
+const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
-      
+      <Head>
+        {/* <!-- Primary Meta Tags --> */}
+        <title>Quiz Alura - Game Of Thrones</title>
+        <meta name="title" content="Quiz Alura - Game Of Thrones" key="title" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+        <meta name="description" content="Teste seus conhecimentos sobre as Crônicas de Gelo e Fogo." key="twitter.description" />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" key="og.type" />
+        <meta property="og:url" content="https://aluraquizgot.vercel.app/" key="og.url" />
+        <meta property="og:title" content="Quiz Alura - Game Of Thrones" key="og.title" />
+        <meta property="og:description" content="Teste seus conhecimentos sobre as Crônicas de Gelo e Fogo." key="og.description" />
+        <meta property="og:image" content="https://ninfacrocodilo.com/wp-content/uploads/2019/05/game-of-thrones-poster_85627-1920x1200.01.jpg" key="og.image" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" key="twitter.card" />
+        <meta property="twitter:url" content="https://aluraquizgot.vercel.app/" key="twitter.url" />
+        <meta property="twitter:title" content="Quiz Alura - Game Of Thrones" key="twitter.title" />
+        <meta property="twitter:description" content="Teste seus conhecimentos sobre as Crônicas de Gelo e Fogo." key="twitter.description" />
+        <meta property="twitter:image" content="https://ninfacrocodilo.com/wp-content/uploads/2019/05/game-of-thrones-poster_85627-1920x1200.01.jpg" key="twitter.image" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/*  eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import Head from '../src/components/Head'
+import styled from 'styled-components';
+import React from 'react';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
 
-
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `;
 
 // const BackgroundImage = styled.div`
 //    background-image: url(${db.bg});
@@ -34,32 +34,55 @@ const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
   return (
-   
 
-  <QuizBackground backgroundImage={db.bg}> 
-  <Head/>
-    <QuizContainer>
-    <Widget>
-      <Widget.Header>
-        <h1>{db.title}</h1>
-      </Widget.Header>
-      <Widget.Content>
-        <p>Lorem ipsum a</p>
-      </Widget.Content>
-    </Widget>
+    <QuizBackground backgroundImage={db.bg}>
 
-    <Widget>
-      <Widget.Header>
-        <h1>Quizes da Galera</h1>
-      </Widget.Header>
-    <Widget.Content>
-      <h1>Quizes da Galera</h1>
-      <p>Lorem ipsum popoppop</p>
-      </Widget.Content>
-    </Widget>
-    <Footer/>
-    </QuizContainer>
-    <GitHubCorner projectUrl=""/>
-  </QuizBackground>)
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+
+              router.push(`/quiz?name=${name}`);
+              // router manda para próxima página
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  // name = infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value);
+                  // State
+                }}
+                placeholder="Coloque seu nome, vassalo! "
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {' '}
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da Galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>Lorem ipsum popoppop</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/garcia-art/aluraquiz" />
+    </QuizBackground>
+  );
 }
